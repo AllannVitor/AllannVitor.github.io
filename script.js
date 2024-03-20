781602d1cd0419d397d5c389bde386ace30074fc
AllannVitor <157865419+AllannVitor@users.noreply.github.com>
2024-03-19 23:42:24 -0300

    Upload do site de portifoli

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggLeMode() {
+  const html = document.documentElement
+
+  // if(html.classList.contains('light')) {
+  //   html.classList.romeve('light')
+  // } else {
+  //   html.classList.add('light')
+  // }
+  html.classList.toggle("light")
+}
